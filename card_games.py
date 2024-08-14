3b14b225a560f556f8c25b037932081bbf55d2d2
Harriet <harriet.mertens@protonmail.com>
2024-08-14 20:37:59 +0100

    added concatenate_rounds function

--- card_games.py
+++ card_games.py
@@ -3,4 +3,11 @@ def get_rounds(round_number):
     return round_output
 
 
-print(get_rounds(27))
+# print(get_rounds(27))
+
+
+def concatenate_rounds(rounds_1, rounds_2):
+    return rounds_1 + rounds_2
+
+
+print(concatenate_rounds([27, 28, 29], [35, 36]))
